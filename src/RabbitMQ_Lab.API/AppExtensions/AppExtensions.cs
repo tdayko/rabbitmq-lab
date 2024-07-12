fbaa9baad3e40053db254d6677a6c60d754df65f
urltapas <ryan.bromati@hotmail.com>
2024-07-11 22:49:47 -0300

    feat: Add RabbitMQ service configuration with MassTransit Lib

---
+++ src/RabbitMQ_Lab.API/AppExtensions/AppExtensions.cs
@@ -0,0 +1,18 @@
+using MassTransit;
+namespace RabbitMQ_Lab.API.AppExtensions;
+
+internal static class AppExtensions
+{
+    public static IServiceCollection AddRabbitMQService(this IServiceCollection services)
+    {
+        services.AddMassTransit(busConfigurator => {
+            busConfigurator.UsingRabbitMq((context, configuration) => configuration.Host(new Uri("amqp://localhost:5672"), host =>
+            {
+                host.Username("guest");
+                host.Password("guest");
+            }));
+        });
+        
+        return services;
+    }
+}
--- src/RabbitMQ_Lab.API/Program.cs
+++ src/RabbitMQ_Lab.API/Program.cs
@@ -1,11 +1,13 @@
+using RabbitMQ_Lab.API.AppExtensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRabbitMQService();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection(); 
+app.UseHttpsRedirection();
 app.UseExceptionHandler("/error");
 app.UseSwagger();
 app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "RabbitMQ_Lab.API v1"));
