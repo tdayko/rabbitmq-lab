8dd1ec31e56c1719d605e963d9b7f33cfd485556
urltapas <ryan.bromati@hotmail.com>
2024-07-14 01:45:21 -0300

    feat: Update Consumer class to use ILogger for logging

--- src/RabbitMQ_Lab/Bus/Consumer..cs
+++ src/RabbitMQ_Lab/Bus/Consumer..cs
@@ -3,24 +3,25 @@ using static RabbitMQ_Lab.Report.Report;
 
 namespace RabbitMQ_Lab.Bus;
 
-internal sealed class Consumer : IConsumer<ReportRequestEvent>
+internal sealed class Consumer(Logger<Consumer> logger) : IConsumer<ReportRequestEvent>
 {
+    private readonly ILogger<Consumer> _logger = logger;
     public async Task Consume(ConsumeContext<ReportRequestEvent> context)
     {
         var report = ReportList.RequestReports.FirstOrDefault(x => x.Id == context.Message.Id);
         if (report is null)
         {
-            Console.Out.WriteLine($"Report with Id {context.Message.Id} not found.");
+            _logger.LogInformation($"Report with Id {context.Message.Id} not found.");
             return;
         }
 
         report.Status = Status.Processing;
-        Console.Out.WriteLine($"Processing report with Id {report.Id} and Name {report.Name}.");
+        _logger.LogInformation($"Processing report with Id {report.Id} and Name {report.Name}.");
 
         await Task.Delay(5000);
 
         report.Status = Status.Processed;
         report.ProcessedTime = DateTime.Now;
-        Console.Out.WriteLine($"Report with Id {report.Id} and Name {report.Name} processed at {report.ProcessedTime}.");
+        _logger.LogInformation($"Report with Id {report.Id} and Name {report.Name} processed at {report.ProcessedTime}.");
     }
 }
