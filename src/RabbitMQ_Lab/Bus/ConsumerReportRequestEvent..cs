0dc9e4617790d82781099194b0435eb141cfc6a6
urltapas <ryan.bromati@hotmail.com>
2024-07-14 20:09:36 -0300

    fix: loggering in the consumer and consumer name not allowed

---
+++ src/RabbitMQ_Lab/Bus/ConsumerReportRequestEvent..cs
@@ -0,0 +1,28 @@
+using MassTransit;
+
+using static RabbitMQ_Lab.Report.Report;
+
+namespace RabbitMQ_Lab.Bus;
+
+internal sealed class ConsumerReportRequestEvent(ILogger<ConsumerReportRequestEvent> logger) : IConsumer<ReportRequestEvent>
+{
+    private readonly ILogger<ConsumerReportRequestEvent> _logger = logger;
+    public async Task Consume(ConsumeContext<ReportRequestEvent> context)
+    {
+        var report = ReportList.RequestReports.FirstOrDefault(x => x.Id == context.Message.Id);
+        if (report is null)
+        {
+            _logger.LogInformation($"Report with Id: {context.Message.Id} not found.");
+            return;
+        }
+
+        report.Status = Status.Processing;
+        _logger.LogInformation($"Processing report with Id: {report.Id} and Name: {report.Name}.");
+
+        await Task.Delay(5000);
+
+        report.Status = Status.Processed;
+        report.ProcessedTime = DateTime.Now;
+        _logger.LogInformation($"Report with Id: {report.Id} and Name: {report.Name} processed at: {report.ProcessedTime}.");
+    }
+}
