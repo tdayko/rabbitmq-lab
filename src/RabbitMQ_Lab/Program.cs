8b3098eb48f23b83e1191dcfd5dca0f27c5e0275
urltapas <ryan.bromati@hotmail.com>
2024-08-13 12:22:24 -0300

    feat:  add Sentry for metrics and log monitoring

--- src/RabbitMQ_Lab/Program.cs
+++ src/RabbitMQ_Lab/Program.cs
@@ -8,14 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRabbitMQService();
-builder.Host.UseSerilog((ctx, cfg) => {
+builder.Host.UseSerilog((ctx, cfg) =>
+{
+    var dir = "logs/RabbitMQ_Lab.json";
+    var dsn = builder.Configuration.GetValue<string>("Sentry:Dsn");
+
     cfg.WriteTo.Console();
-    cfg.WriteTo.File(new JsonFormatter(),"logs/RabbitMQ_Lab.json", rollingInterval: RollingInterval.Day);
+    cfg.WriteTo.File(new JsonFormatter(renderMessage: true), dir, rollingInterval: RollingInterval.Day);
+    cfg.WriteTo.Sentry(options =>
+    {
+        options.Dsn = dsn;
+        options.Debug = true;
+        options.MinimumEventLevel = Serilog.Events.LogEventLevel.Debug;
+        options.TracesSampleRate = 1.0;
+    });
 });
 
 var app = builder.Build();
 
-app.AddEndpointApi();   
+app.AddEndpointApi();
 app.UseSwagger();
 app.UseExceptionHandler("/error");
 app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "RabbitMQ_Lab v1"));
