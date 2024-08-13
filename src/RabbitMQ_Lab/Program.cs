055ce4b28c30cc82d525a3db2414e5227d91c034
urltapas <ryan.bromati@hotmail.com>
2024-08-12 21:25:49 -0300

    chore: add serilog and log configuration

--- src/RabbitMQ_Lab/Program.cs
+++ src/RabbitMQ_Lab/Program.cs
@@ -1,12 +1,23 @@
 using RabbitMQ_Lab.AppExtensions;
 using RabbitMQ_Lab.Endpoints;
 
+using Serilog;
+using Serilog.Formatting.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddRabbitMQService();
-builder.Services.AddLogging(log => log.AddJsonConsole());
+builder.Services.AddSerilog();
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    var dir = $"{Environment.CurrentDirectory}/log.json";
+
+    lc.WriteTo.Console();
+    lc.WriteTo.File(new JsonFormatter(), dir, fileSizeLimitBytes: 1024);
+    lc.CreateLogger();
+});
 
 var app = builder.Build();
 
